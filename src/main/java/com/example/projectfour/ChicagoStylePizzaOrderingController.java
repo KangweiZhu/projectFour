990fb986b1cb8bc8d67d4755b884cfba585032f6
anicaaovo <93107633+anicaaovo@users.noreply.github.com>
2022-11-16 19:18:58 -0500

    images added

---
+++ src/main/java/com/example/projectfour/ChicagoStylePizzaOrderingController.java
@@ -0,0 +1,4 @@
+package com.example.projectfour;
+
+public class ChicagoStylePizzaOrderingController {
+}
--- src/main/java/com/example/projectfour/MainViewController.java
+++ src/main/java/com/example/projectfour/MainViewController.java
@@ -1,4 +1,88 @@
 package com.example.projectfour;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MainViewController {
+    @FXML
+    void clickChicagoStyle(ActionEvent event){
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(MainViewController.class.getResource("ChicagoStylePizzaOrdering" +
+                "View.fxml"));
+        Parent root;
+        Scene scene;
+        try {
+            root = fxmlLoader.load();
+            scene = new Scene(root);
+            stage.setResizable(false);
+            stage.setTitle("Chicago Style Pizza");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void clickNYStyle(ActionEvent event){
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(MainViewController.class.getResource("ChicagoStylePizzaOrdering" +
+                "View.fxml"));
+        Parent root;
+        Scene scene;
+        try {
+            root = fxmlLoader.load();
+            scene = new Scene(root);
+            stage.setResizable(false);
+            stage.setTitle("Chicago Style Pizza");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void clickChicagoStyle(ActionEvent event){
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(MainViewController.class.getResource("ChicagoStylePizzaOrdering" +
+                "View.fxml"));
+        Parent root;
+        Scene scene;
+        try {
+            root = fxmlLoader.load();
+            scene = new Scene(root);
+            stage.setResizable(false);
+            stage.setTitle("Chicago Style Pizza");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void clickChicagoStyle(ActionEvent event){
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(MainViewController.class.getResource("ChicagoStylePizzaOrdering" +
+                "View.fxml"));
+        Parent root;
+        Scene scene;
+        try {
+            root = fxmlLoader.load();
+            scene = new Scene(root);
+            stage.setResizable(false);
+            stage.setTitle("Chicago Style Pizza");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
